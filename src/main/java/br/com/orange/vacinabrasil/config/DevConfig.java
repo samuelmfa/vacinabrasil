298656a6a8f56b7a41c2161cda090f66132b07f7
Samuel Costa <samuelmfa@gmail.com>
2021-03-10 18:47:17 -0300

    ajuste no aplication

--- src/main/java/br/com/orange/vacinabrasil/config/DevConfig.java
+++ src/main/java/br/com/orange/vacinabrasil/config/DevConfig.java
@@ -2,24 +2,21 @@ package br.com.orange.vacinabrasil.config;
 
 import java.text.ParseException;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
-import br.com.orange.vacinabrasil.service.DBService;
 
 @Configuration
 @Profile("dev")
 public class DevConfig {
 
-	@Autowired
-	private DBService dbService;
+	// @Autowired
+	// private DBService dbService;
 
 	@Bean
 	public boolean instanciateDataBase() throws ParseException {
 
-		//dbService.instanciateDatabase();
+		// dbService.instanciateDatabase();
 		return true;
 	}
 
